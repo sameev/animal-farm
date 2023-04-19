51dae1080f5d4e8f00cc729fb9915116ce821aa8
Samee Vohra <samee.vohra@gmail.com>
2023-04-19 16:05:12 -0700

    added localstorage to cache search query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Animal from "./Animal";
 
 function App() {
 
   const [animals, setAnimals] = useState([]);
 
+  useEffect( () => {
+    const lastQuery = localStorage.getItem('lastQuery');
+    search(lastQuery);
+  }, [])
+
   const search = async(q) => {
     const res = await fetch(`http://localhost:8080?${new URLSearchParams({q})}`);
     const data = await res.json();
     setAnimals(data);
     if(q === '') setAnimals([]);
+
+    localStorage.setItem('lastQuery', q);
   }
 
 
