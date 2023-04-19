c5679d463d7714f13f0de9c5abf75ca932a6a944
Samee Vohra <samee.vohra@gmail.com>
2023-04-19 15:28:58 -0700

    set up app component for input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react"
 
 function App() {
 
+  const [animals, setAnimals] = useState([]);
+
+
+  const handleTyping = (e) => {
+    console.log(e.target.value)
+  }
+
   return (
     <div className="App">
+      <h1>Animal Farm</h1>
+      <input 
+        type="text"
+        placeholder="Search"
+        onChange={handleTyping}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <li key={animal.id}>
+            <strong>{animal.type}</strong> {animal.name} {animal.age}
+          </li>
+        ))}
+
+        {animals.length === 0 ? 'No Animals Found' : null}
+      </ul>
 
     </div>
   )
